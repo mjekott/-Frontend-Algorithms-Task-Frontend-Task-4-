9e93182053ae35a69afb570503f1547e571724db
mjekott <ekottmfon@yahoo.com>
2021-05-18 14:48:49 +0100

    final solution

--- checkYuGiOh.js
+++ checkYuGiOh.js
@@ -1,5 +1,29 @@
-function checkYuGiOh() {
-  return;
+function checkYuGiOh(n) {
+  if (isNaN(+n)) {
+    return `invalid parameter: "${n}"`;
+  }
+  const arr = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0 && i % 5 === 0) {
+      arr.push('yu-oh');
+    } else if (i % 2 === 0 && i % 3 === 0) {
+      arr.push('yu-gi');
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      arr.push('gi-oh');
+    } else if (i % 2 === 0) {
+      arr.push('yu');
+    } else if (i % 3 === 0) {
+      arr.push('gi');
+    } else if (i % 5 === 0) {
+      arr.push('oh');
+    } else {
+      arr.push(i);
+    }
+  }
+
+  return `${JSON.stringify(arr)}`;
 }
 
-module.exports = checkYuGiOh;
+console.log(checkYuGiOh(10));
+console.log(checkYuGiOh(5));
+console.log(checkYuGiOh('fizzbuzz is meh'));
--- convertFahrToCelsius.js
+++ convertFahrToCelsius.js
@@ -1,5 +1,18 @@
-function convertFahrToCelsius() {
-  return;
+function convertFahrToCelsius(parameter) {
+  if (Array.isArray(parameter))
+    return `${JSON.stringify(parameter)} is not a valid number but a/an array.`;
+
+  if (isNaN(+parameter))
+    return `${JSON.stringify(
+      parameter
+    )} is not a valid number but a/an ${typeof parameter}.`;
+
+  const result = ((parameter - 32) * 5) / 9;
+
+  return `${result.toFixed(4)}`;
 }
 
-module.exports = convertFahrToCelsius;
+console.log(convertFahrToCelsius(0));
+console.log(convertFahrToCelsius('0'));
+console.log(convertFahrToCelsius([1, 2, 3]));
+console.log(convertFahrToCelsius({ temp: 0 }));
